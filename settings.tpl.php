e163ef821d18e6ee64a60175e0f9b4844248b7a1
keyword-strategy <keyword-strategy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-20 13:34:18 +0000

    tagging version 1.8.8
    git-svn-id: https://plugins.svn.wordpress.org/keyword-strategy-internal-links/tags/1.8.8@492633 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.tpl.php
+++ settings.tpl.php
@@ -1,5 +1,15 @@
 <div class="wrap">
-<h2>Keyword Strategy</h2>
+<h2 style="float:left;">Keyword Strategy</h2>
+
+<h2 class="nav-tab-wrapper">
+<a class="nav-tab nav-tab-active" href="<?= KWS_PLUGIN_URL ?>">Overview</a>
+	<a class="nav-tab" href="<?= KWS_PLUGIN_URL . '&kws_action=inpage' ?>">Insert Keywords</a>
+	<a class="nav-tab" href="<?= KWS_PLUGIN_URL . '&kws_action=related' ?>">Links Needed</a>
+</h2>
+
+<? if (! function_exists('get_admin_url')): ?>
+<p style="color:red;">Your WordPress version is not supported. Please update.</p>
+<? endif; ?>
 
 <? if ($kws_options['username'] && !isset($_GET['kws_login_error'])): ?>
 <p>Current login: <b><?= htmlspecialchars($kws_options['username']) ?></b> <input type="button" class="button" value="Change login" style="margin-top: 15px;" onclick="document.getElementById('kws-login-box').style.display='block';this.parentNode.style.display='none';" /></p>
@@ -44,6 +54,10 @@
 			<td><input name="kws_tracker_enabled" type="checkbox" id="kws_tracker_enabled" value="1" <?= isset($kws_options['tracker_enabled']) && !$kws_options['tracker_enabled']? '': 'checked="checked"' ?> /> </td> 
 		</tr>
 		<tr valign="top"> 
+			<th scope="row"><label for="kws_header_links" style="white-space:nowrap;"> Allow links in H1-H6 tags</label></th> 
+			<td><input name="kws_header_links" type="checkbox" id="kws_header_links" value="1" <?= ! $kws_options['header_links']? '': 'checked="checked"' ?> /> </td> 
+		</tr>
+		<tr valign="top"> 
 			<th scope="row"><label for="kws_wait_days" style="white-space:nowrap;"> New articles shouldn't show links for how many days</label></th> 
 			<td><input name="kws_wait_days" type="text" id="kws_wait_days" value="<?= $kws_options['wait_days'] ?>" class="small-text" /> default: 0</td> 
 		</tr>
@@ -63,21 +77,39 @@
 			</td> 
 		</tr>
 		<tr valign="top"> 
-			<th scope="row"><label style="white-space:nowrap;"> Maximum number of keywords to link sitewide</label></th> 
+			<th scope="row"><label style="white-space:nowrap;" for="kws_keywords_limit"> Maximum number of keywords to link sitewide</label></th> 
 			<td>
-				<select name="kws_keywords_limit">
+				<select name="kws_keywords_limit" id="kws_keywords_limit">
 					<? foreach ($keywords_limits AS $limit): ?>
-						<option value="<?= $limit ?>" <? if($limit == $kws_options['keywords_limit'] || (!isset($kws_options['keywords_limit']) && $limit == 10000)): ?>selected="selected"<? endif; ?> ><?= $limit ?></option>
+						<option value="<?= $limit ?>" <? if($limit == $kws_options['keywords_limit'] || (!isset($kws_options['keywords_limit']) && $limit == 1000)): ?>selected="selected"<? endif; ?> ><?= $limit ?></option>
 					<? endforeach; ?>
 				</select>
-				default: 10000
+				<b>If you are expereincing any performance issues, try to lower this.</b> default: 1000
 			</td> 
+		</tr>
+		<tr valign="top">
+			<th scope="row"><label style="white-space:nowrap;" for="kws_banned_urls"> Disable plugin on these pages:</label></th>
+			<td>
+				<div style="float:left; margin-right: 5px;">
+					<textarea name="kws_banned_urls" rows="4" cols="40" wrap="off" id="kws_banned_urls"><?= htmlspecialchars($kws_options['banned_urls']) ?></textarea>
+				</div>
+				<div>
+					Examples:<br />
+					<b>/gallery/</b> will match all pages with /gallery/ inside.<br />
+					<b>http://www.example.com/341-article</b> will match single article.<br />
+					<b>/site/*/content/</b> will match /site/nature/content/, /site/politics/content/, /site/tech/content, etc.
+				</div>
+			</td>
 		</tr>
 	</table>
 	<p class="submit"> 
 		<input type="submit" name="Submit" class="button-primary" value="Save Changes" /> 
 	</p> 
 </form>
+
+<p>
+	Tip: You can put some text inside <b><i>&lt;kwsignore&gt;&lt;/kwsignore&gt;</i></b> tags to avoid plugin inserting links inside.
+</p>
 <? endif; ?>
 
 </div>
